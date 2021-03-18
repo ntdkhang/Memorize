83c7560fd26c7fc20d39eec84df27b4f5803bf07
DKhang <ntdkhang143@gmail.com>
2021-03-18 20:14:08 +0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Nguyen Tran Duy Khang on 3/18/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
